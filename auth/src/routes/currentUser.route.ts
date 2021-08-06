8c5b2d0e3f1d6260491712dc8ca0c90fd3e32bcd
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-07 01:46:04 +0430

    Add user middleware

--- auth/src/routes/currentUser.route.ts
+++ auth/src/routes/currentUser.route.ts
@@ -1,20 +1,14 @@
+import { UserMiddleware } from './../middlewares/user.middleware';
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  '/api/users/current-user',
+  UserMiddleware,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.user || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-    res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as CurrentUserRouter };
