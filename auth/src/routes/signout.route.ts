a5376085e30229bf846e121c6da41675a0185819
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-07 01:38:00 +0430

    Add signout api

--- auth/src/routes/signout.route.ts
+++ auth/src/routes/signout.route.ts
@@ -3,8 +3,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
+  req.session = null;
   res.send({});
 });
 
