62e1cc5280acad5b8bc319ea7abb52c5ea7fdb9f
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-18 22:55:01 +0430

    Add tickets depl, cip.srv and variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,14 +1,17 @@
 import 'express-async-errors';
 import mongoose from 'mongoose';
-import {app} from './app'
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWt Key is not provided!');
+    throw new Error('JWT Key is not provided!');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI is not provided!');
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-cip-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -19,7 +22,7 @@ const start = async () => {
   }
 
   app.listen(3000, () =>
-    console.log('Auth microservice is listening on port 3000!')
+    console.log('Ticket microservice is listening on port 3000!')
   );
 };
 
