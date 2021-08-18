9aa5b2c951b67898665384770ad058ded4f54921
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-18 22:57:04 +0430

    Add mongo uri to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,17 @@
 import 'express-async-errors';
 import mongoose from 'mongoose';
-import {app} from './app'
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWt Key is not provided!');
+    throw new Error('JWT Key is not provided!');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI is not provided!');
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-cip-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
