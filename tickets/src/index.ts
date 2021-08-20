e45074e5364b325cd70b1077d5301746ffe7cce0
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-20 23:11:01 +0430

    Modify to add nats connection close handler

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,15 @@ const start = async () => {
 
   try {
     await natsWrapper.connect('ticketing', 'ljsdf', 'http://nats-cip-srv:4222');
+
+    natsWrapper.client!.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
