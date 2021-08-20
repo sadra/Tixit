ef13245f4f5815fa3408aff27db06c72e90cd815
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-20 22:04:18 +0430

    Add natsWrapper client

---
+++ tickets/wrappers/nats.wrapper.ts
@@ -0,0 +1,22 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  connect(clusterId: string, clientId: string, url: string): Promise<void> {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this._client!.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+
+      this._client!.on('error', (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
