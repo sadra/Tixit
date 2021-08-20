86ae55b481304fb1a6019143bbfa638cf3d06c31
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-20 22:07:25 +0430

    Modify to add client getter

--- tickets/wrappers/nats.wrapper.ts
+++ tickets/wrappers/nats.wrapper.ts
@@ -3,6 +3,14 @@ import nats, { Stan } from 'node-nats-streaming';
 class NatsWrapper {
   private _client?: Stan;
 
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access to client before nats connecting');
+    }
+
+    return this._client;
+  }
+
   connect(clusterId: string, clientId: string, url: string): Promise<void> {
     this._client = nats.connect(clusterId, clientId, { url });
 
