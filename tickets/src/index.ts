f8fec509ddff845b59776570588c1c95dc88b6a3
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-20 23:59:47 +0430

    Add NATS envs

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -10,9 +10,22 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI is not provided!');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID is not provided!');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL is not provided!');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID is not provided!');
+  }
 
   try {
-    await natsWrapper.connect('ticketing', 'ljsdf', 'http://nats-cip-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
     natsWrapper.client!.on('close', () => {
       console.log('NATS connection closed!');
