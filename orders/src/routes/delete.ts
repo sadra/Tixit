365534d4d9c123a0b85ece30a726e608d60bc4da
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-24 02:02:01 +0430

    Add delete an orders route and tests

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  RequireAuthMiddleware,
+} from '@tixit/common';
+import {Order, OrderStatus} from '../models/order'
 
 const router = express.Router();
 
-router.delete('/api/orders/:id', async (req: Request, res: Response) => {
-  res.status(200).send();
+router.delete('/api/orders/:id',
+  RequireAuthMiddleware,
+async (req: Request, res: Response) => {
+  const order = await Order
+    .findById(req.params.id);
+
+  if(!order){
+    throw new NotFoundError();
+  }
+
+  if(order.userId !== req.user!.id){
+    throw new NotAuthorizedError()
+  }
+
+  order.status = OrderStatus.CANCELLED
+  await order.save()
+
+  res.status(204).send();
 });
 
 export { router as deleteOrderRouter };
