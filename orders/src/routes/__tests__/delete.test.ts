7dd738a8d196e6ad60aeab0aaeef932a1a003a8a
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-24 02:02:47 +0430

    Modify to fix unimported lib

--- orders/src/routes/__tests__/delete.test.ts
+++ orders/src/routes/__tests__/delete.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import {app} from '../../app'
 import {Ticket} from '../../models/ticket'
-import {OrderStatus} from '../../models/order'
+import {Order, OrderStatus} from '../../models/order'
 
 const buildTicket= async () => {
     const ticket = Ticket.build({
