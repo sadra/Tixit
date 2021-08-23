aa3335d040035e373a09efb37dd867dbfcfc8fdf
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-24 02:01:18 +0430

    Add show all orders route and tests

---
+++ orders/src/routes/__tests__/index.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest'
+import {app} from '../../app'
+import mongoose from 'mongoose'
+import {Ticket} from '../../models/ticket'
+import {Order, OrderStatus} from '../../models/order'
+
+const buildTicket= async () => {
+    const ticket = Ticket.build({
+        title: 'Concert',
+        price: 20
+    })
+    await ticket.save()
+  
+    return ticket
+}
+
+describe('Get All Orders', () => {
+  it('should fetchers orders for and particular user', async () => {
+    const ticketOne = await buildTicket()
+    const ticketTwo = await buildTicket()
+    const ticketThree = await buildTicket()
+
+    const userOne = global.signin()
+    const userTwo = global.signin()
+
+    await request(app)
+      .post("/api/orders")
+      .set('Cookie', userOne)
+      .send({ticketId: ticketOne.id})
+      .expect(201);
+
+    const {body: orderOne} = await request(app)
+        .post("/api/orders")
+        .set('Cookie', userTwo)
+        .send({ticketId: ticketTwo.id})
+        .expect(201);
+    const {body: orderTwo} = await request(app)
+        .post("/api/orders")
+        .set('Cookie', userTwo)
+        .send({ticketId: ticketThree.id})
+        .expect(201);
+
+    const response = await request(app)
+        .get("/api/orders")
+        .set('Cookie', userTwo);
+
+    expect(response.body.length).toEqual(2)
+    expect(response.body[0].id).toEqual(orderOne.id)
+    expect(response.body[1].id).toEqual(orderTwo.id)
+    expect(response.body[0].ticket.id).toEqual(ticketTwo.id)
+    expect(response.body[1].ticket.id).toEqual(ticketThree.id)
+  });
+});
--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
+import {
+  RequireAuthMiddleware,
+} from '@tixit/common';
+import {Order} from '../models/order'
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.status(200).send();
+router.get('/api/orders',
+  RequireAuthMiddleware,
+async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.user!.id
+  }).populate('ticket');
+
+  res.send(orders);
 });
 
 export { router as indexOrdersRouter };
