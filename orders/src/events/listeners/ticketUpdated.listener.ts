f471372bef0f8eaa04b77391da8c7d98797691d8
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-25 01:14:34 +0430

    Add ticket created/updated listeners

---
+++ orders/src/events/listeners/ticketUpdated.listener.ts
@@ -0,0 +1,24 @@
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { Listener, Subjects, TicketUpdatedEvent } from '@tixit/common';
+import { Ticket } from '../../models/ticket';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
