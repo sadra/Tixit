05c366eaa7d80b20f1bffc74f1d6b84f96a84ca9
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-27 18:56:46 +0430

    formated codes

--- common/src/event-bus/listener.abstract.ts
+++ common/src/event-bus/listener.abstract.ts
@@ -1,10 +1,10 @@
-import { Message, Stan } from "node-nats-streaming";
-import { Event } from "./events/event.interface";
+import { Message, Stan } from 'node-nats-streaming';
+import { Event } from './events/event.interface';
 
 export abstract class Listener<T extends Event> {
-  abstract subject: T["subject"];
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: T["data"], msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   private client: Stan;
 
   protected ackWait = 5 * 1000;
@@ -29,7 +29,7 @@ export abstract class Listener<T extends Event> {
       this.subscriptionOptions()
     );
 
-    subscription.on("message", (msg: Message) => {
+    subscription.on('message', (msg: Message) => {
       console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
 
       const parsedData = this.parseMessage(msg);
@@ -41,8 +41,8 @@ export abstract class Listener<T extends Event> {
   parseMessage(msg: Message): any {
     const data = msg.getData();
 
-    return typeof data === "string"
+    return typeof data === 'string'
       ? JSON.parse(data)
-      : JSON.parse(data.toString("utf8"));
+      : JSON.parse(data.toString('utf8'));
   }
 }
--- common/src/event-bus/publisher.abstract.ts
+++ common/src/event-bus/publisher.abstract.ts
@@ -1,22 +1,22 @@
-import { Event } from "./events/event.interface";
-import { Stan } from "node-nats-streaming";
+import { Event } from './events/event.interface';
+import { Stan } from 'node-nats-streaming';
 
 export abstract class Publisher<T extends Event> {
-  abstract subject: T["subject"];
+  abstract subject: T['subject'];
   private client: Stan;
 
   constructor(client: Stan) {
     this.client = client;
   }
 
-  publish(data: T["data"]): Promise<void> {
+  publish(data: T['data']): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err) => {
         if (err) {
           reject(err);
         }
 
-        console.log("Event published to the subject", this.subject);
+        console.log('Event published to the subject', this.subject);
 
         resolve();
       });
