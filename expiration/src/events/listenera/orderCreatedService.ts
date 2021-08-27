0af4a1268224e5e839d548af2c20a9c845a543e2
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-27 21:51:39 +0430

    Modify to add delay

--- expiration/src/events/listenera/orderCreatedService.ts
+++ expiration/src/events/listenera/orderCreatedService.ts
@@ -8,9 +8,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
 
     msg.ack();
   }
