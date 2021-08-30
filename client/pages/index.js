661ca3b759f8abe0ede244ed59586c57c4e6ba47
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-31 00:34:49 +0430

    Modify to add list of tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,35 @@
-import axios from 'axios';
-import buildClient from '../api/build-client';
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
 
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are Signed in</h1>
-  ) : (
-    <h1>You are NOT Signed in</h1>
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async ({
-  context, client, currentUser
-}) => {
-  // const client = buildClient(context);
+LandingPage.getInitialProps = async ({ context, client, currentUser }) => {
+  const { data } = await client.get("/api/tickets");
 
-  // const { data } = await client.get(`/api/users/currentuser`).catch((err) => {
-  //   console.log(err.message);
-  // });
-
-  // return data;
-
-  return {}
+  return {
+    tickets: data,
+  };
 };
 
 export default LandingPage;
