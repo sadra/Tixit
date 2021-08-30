3785c040f7f213669bcb1c8110dbe1b9bf74346a
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-08-31 00:28:26 +0430

    Modify to add send request to create ticket

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,16 +1,57 @@
+import {useState} from 'react'
+import useRequest from '../../hooks/use-request'
+import Router from 'next/router'
+
 const NewTicket = () => {
+    const [title, setTitle] = useState('');
+    const [price, setPrime] = useState('');
+    const {doRequest, errors} = useRequest({
+        url: '/api/tickets',
+        method: 'post',
+        body: {
+            title, price
+        },
+        onSuccess: (ticket) => Router.push('/')
+    })
+
+    const onBlur = () => {
+        const value = parseFloat(price)
+
+        if(isNaN(value)){
+            return;
+        }
+
+        setPrice(value.toFixed(2))
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        doRequest();
+    }
+
     return (
         <div>
             <h1>Create a Ticket</h1>
-            <from>
+            <from onSumbit={onSubmit}>
                 <div className="form-group">
                     <label>Title</label>
-                    <input className="form-control" />
+                    <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="form-control"
+                    />
                 </div>
                 <div className="form-group">
                     <label>Price</label>
-                    <input className="form-control" />
+                    <input
+                        value={price}
+                        onBlur={onBlur}
+                        onChange={(e) => setPrime(e.target.value)}
+                        className="form-control"
+                    />
                 </div>
+                {errors}
                 <button className="btn btn-primary">Submit</button>
             </from>
         </div>
