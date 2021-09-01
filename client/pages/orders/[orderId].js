c7c7ce01d44145ac41c4d46843d54635651a8898
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-09-01 19:54:22 +0430

    Add order pay button page

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const OrderShow = () => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  if(timeLeft < 0){
+      return <div>Order Expired</div>
+  }
+
+  return <div>Time left to pay: ${timeLeft} seconds</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
